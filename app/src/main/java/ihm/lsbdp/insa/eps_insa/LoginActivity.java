b0101f32b2237aea151567cda92f83bb2868dfec
Corentin Giraud <corentin.giraud@insa-lyon.fr>
2017-11-10 00:31:26 +0100

    Functional

--- app/src/main/java/ihm/lsbdp/insa/eps_insa/LoginActivity.java
+++ app/src/main/java/ihm/lsbdp/insa/eps_insa/LoginActivity.java
@@ -1,7 +1,12 @@
 package ihm.lsbdp.insa.eps_insa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void connectStudentBefore(View view) {
+        Intent intent = new Intent(this, StudentBeforeActivity.class);
+        startActivity(intent);
+    }
+
+    public void connectStudentAfter(View view) {
+        Intent intent = new Intent(this, StudentAfterActivity.class);
+        startActivity(intent);
+    }
+
+    public void connectTeacher(View view) {
+        Intent intent = new Intent(this, TeacherActivity.class);
+        startActivity(intent);
     }
 }
