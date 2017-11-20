cc12cadb3d27ee175b59eb2850c44120ec847c24
Piet <vincent.piet@nordnet.fr>
2017-11-20 16:27:16 +0100

    Scroll + ListView en cours

--- app/src/main/java/ihm/lsbdp/insa/eps_insa/teacher/SlotTeacherActivity.java
+++ app/src/main/java/ihm/lsbdp/insa/eps_insa/teacher/SlotTeacherActivity.java
@@ -1,15 +1,33 @@
 package ihm.lsbdp.insa.eps_insa.teacher;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import ihm.lsbdp.insa.eps_insa.R;
 
-public class SlotTeacherActivity extends AppCompatActivity {
+public class SlotTeacherActivity extends Activity{
+
+    private ListView mListView;
+    private String[] prenoms = new String[]{
+            "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
+            "Gerard", "Hugo", "Ingrid", "Jonathan", "Kevin", "Logan",
+            "Mathieu", "Noemie", "Olivia", "Philippe", "Quentin", "Romain",
+            "Sophie", "Tristan", "Ulric", "Vincent", "Willy", "Xavier",
+            "Yann", "Zoé"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slot_teacher);
+
+        mListView = (ListView) findViewById(R.id.student_list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(SlotTeacherActivity.this,
+                android.R.layout.simple_list_item_1, prenoms);
+        mListView.setAdapter(adapter);
     }
 }
