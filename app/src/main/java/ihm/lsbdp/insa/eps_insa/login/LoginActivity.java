691f383ec15319afff0306250cffe74b8dc1b2e6
P4py <matthieu.valvin@gmail.com>
2017-11-20 16:46:32 +0100

    Merge branch 'master' into Papyz

--- app/src/main/java/ihm/lsbdp/insa/eps_insa/login/LoginActivity.java
+++ app/src/main/java/ihm/lsbdp/insa/eps_insa/login/LoginActivity.java
@@ -1,9 +1,12 @@
 package ihm.lsbdp.insa.eps_insa.login;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
 
 import ihm.lsbdp.insa.eps_insa.R;
 import ihm.lsbdp.insa.eps_insa.student.after.StudentAfterActivity;
@@ -12,24 +15,31 @@ import ihm.lsbdp.insa.eps_insa.teacher.TeacherActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText mEdit;
+    String toTest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEdit = (EditText) findViewById(R.id.login_input_ID);
     }
 
-    public void connectStudentBefore(View view) {
-        Intent intent = new Intent(this,StudentBeforeActivity.class);
-        startActivity(intent);
-    }
+    public void connect(View view) {
+        toTest = mEdit.getText().toString();
 
-    public void connectStudentAfter(View view) {
-        Intent intent = new Intent(this,StudentAfterActivity.class);
-        startActivity(intent);
-    }
-
-    public void connectTeacher(View view) {
-        Intent intent = new Intent(this,TeacherActivity.class);
-        startActivity(intent);
+        if (toTest.equals("agaillot")) {
+            Intent intent = new Intent(this, StudentBeforeActivity.class);
+            startActivity(intent);
+        } else if (toTest.equals("bros")) {
+            Intent intent = new Intent(this, StudentAfterActivity.class);
+            startActivity(intent);
+        } else if (toTest.equals("hbonisseur")) {
+            Intent intent = new Intent(this, TeacherActivity.class);
+            startActivity(intent);
+        } else {
+            Snackbar.make(view, "Log in error", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+        }
     }
 }
