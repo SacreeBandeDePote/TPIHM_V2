c43110337d08031489ae6e71841803720db671d4
Ros <rosbenjamindu01@gmail.com>
2017-11-20 20:42:59 +0100

    ROS_JOB_DONE

--- app/src/main/java/ihm/lsbdp/insa/eps_insa/teacher/SlotTeacherActivity.java
+++ app/src/main/java/ihm/lsbdp/insa/eps_insa/teacher/SlotTeacherActivity.java
@@ -1,33 +1,28 @@
 package ihm.lsbdp.insa.eps_insa.teacher;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import ihm.lsbdp.insa.eps_insa.R;
 
-public class SlotTeacherActivity extends Activity{
+public class SlotTeacherActivity extends AppCompatActivity{
 
-    private ListView mListView;
-    private String[] prenoms = new String[]{
-            "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
-            "Gerard", "Hugo", "Ingrid", "Jonathan", "Kevin", "Logan",
-            "Mathieu", "Noemie", "Olivia", "Philippe", "Quentin", "Romain",
-            "Sophie", "Tristan", "Ulric", "Vincent", "Willy", "Xavier",
-            "Yann", "Zoé"
-    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slot_teacher);
-
-        mListView = (ListView) findViewById(R.id.student_list);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(SlotTeacherActivity.this,
-                android.R.layout.simple_list_item_1, prenoms);
-        mListView.setAdapter(adapter);
     }
+
+    public void slotLauncher(View view) {
+        Intent intent = new Intent(this,listEleveActivity.class);
+        startActivity(intent);
+    }
+
 }
